a971031b35f752c16c634c4df751e7b79992aa6d
Samat <iron.flex@yandex.ru>
2020-05-18 16:34:52 +0300

    Add Angular  validation login/registration forms

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -6,17 +6,15 @@ validationApp.controller('mainController', function($scope) {
 
     // function to submit the form after all validation has occurred
     $scope.submitForm = function() {
-
         // check to make sure the form is completely valid
         if ($scope.userForm.$valid) {
             alert('Successfully');
         }
     };
 });
-ngModel.$validators.validCharacters = function(modelValue, viewValue) {
-    var value = modelValue || viewValue;
-    return /[0-9]+/.test(value) &&
-        /[a-z]+/.test(value) &&
-        /[A-Z]+/.test(value) &&
-        /\W+/.test(value);
-};
+new TypeIt("#callback", {
+    strings: "Product List",
+    speed: 60,
+    waitUntilVisible: true,
+
+}).go();
